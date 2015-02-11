e0941767edc6ac6445ed6f0450b38f4972461856
Casper Beyer <caspervonb@qup.io>
2015-02-12 03:37:57 +0800

    Initial commit

---
+++ cmd.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+var through = require('through2');
+var chrome = require('chrome-remote-interface');
+var path = require('path');
+var http = require('http');
+
+var scripts = {};
+
+var client = chrome(function(client) {
+  client.on('error', function() {
+  });
+
+  console.log(JSON.stringify({
+    time:new Date(),
+    level: 'info',
+    message: 'client connected on port 9222',
+  }));
+
+  process.stdin
+    .pipe(through(parse))
+    .pipe(process.stdout);
+
+    function parse(buf, enc, next) {
+    try {
+      var data = JSON.parse(buf);
+      if (data.type == 'change') {
+        var script = scripts[data.url];
+
+        if (script) {
+          http.get(script.url, function(res) {
+            res.setEncoding('utf-8');
+            res.on('data', function (data) {
+              var params = {
+                scriptId: script.scriptId,
+                scriptSource: data.toString(),
+              };
+
+              client.Debugger.setScriptSource(params, function(error, response) {
+                if (error) {
+                  return console.log(JSON.stringify({
+                    time:new Date(),
+                    level: 'error',
+                    message: response.message,
+                  }));
+                }
+
+                console.log(JSON.stringify({
+                  time:new Date(),
+                  level: 'info',
+                  event: 'source',
+                  url: data.url,
+                }));
+              });
+            });
+          });
+        }
+      }
+    } catch (err) {
+      // no-op
+    }
+
+    this.push(buf);
+    next();
+  }
+
+  client.on('Debugger.scriptParsed', function(params) {
+    if (params.url.indexOf('http://') > -1) {
+
+      scripts[path.basename(params.url)] = params;
+      console.log(JSON.stringify({
+        time:new Date(),
+        level: 'info',
+        event: 'parse',
+        url: params.url
+      }));
+    }
+  });
+
+  client.Debugger.enable();
+});
